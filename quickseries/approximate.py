18fadcd9b5127d212c32410ec1fb5470cdcfd7e1
m-stclair <mstclair@millionconcepts.com>
2023-12-19 03:15:00 -0500

    add comments

--- quickseries/approximate.py
+++ quickseries/approximate.py
@@ -1,11 +1,10 @@
 from inspect import getfullargspec
 from itertools import chain, count
 import re
-from typing import Callable, Union, Any, Optional, Sequence, Literal
+from typing import Any, Callable, Literal, Optional, Sequence, Union
 
 from cytoolz import groupby
 from dustgoggles.dynamic import compile_source, define, getsource
-from dustgoggles.structures import listify
 import numpy as np
 import sympy as sp
 
@@ -20,6 +19,7 @@ EXP_PATTERN = re.compile(r"\w+ ?\*\* ?(\d+)")
 SIMPLE_POLY_ELEMENTS = (
     sp.Add, sp.Mul, sp.Number, sp.Symbol, sp.Pow, sp.Integer
 )
+"""sympy classes that may appear in a 'simple' (by our standards) polynomial"""
 
 
 def is_simple_poly(expr: sp.Expr) -> bool:
@@ -63,6 +63,7 @@ def series_lambda(
                 syms.append(coefficient)
             else:
                 outargs.append(a)
+        # check for things like 'cos(2)' and try to turn them into numbers
         elif isinstance((number := a.evalf()), sp.Number):
             outargs.append(number)
         else:
@@ -76,6 +77,7 @@ def series_lambda(
 
 
 def lastline(func: Callable) -> str:
+    """try to get the last line of a function, sans return statement"""
     return tuple(
         filter(None, getsource(func).split("\n"))
     )[-1].replace("return", "").strip()
@@ -89,36 +91,42 @@ def regexponents(text: str) -> tuple[int]:
 def optimize_exponents(
     exps: Sequence[int]
 ) -> tuple[dict[int, list[int]], dict[int, list[int]]]:
+    # list of tuples like: (factor, [factors to use in decomposition])
     replacements = [(e, [e]) for e in exps]
+    # which factors have we already assessed?
     reduced = set()
+    # which factors have we used in decompositions?
     extant = tuple(chain(*[r[1] for r in replacements]))
     while True:
-        if len(extant) == 1:
+        if len(extant) == 1:  # trivial case
             replacements[0][1][:] = [1 for _ in range(replacements[0][0])]
             break
         counts = {
-            k: len(v)
-            for k, v in groupby(lambda x: x, extant).items()
+            k: len(v) for k, v in groupby(lambda x: x, extant).items()
             if k not in reduced
         }
-        if len(counts) < 2:
+        if len(counts) < 2:  # nothing useful left to do
             break
         elif counts[max(counts)] > 1:
+            # don't decompose the biggest factor; because it appears more than
+            # once, we'd like to precompute it
             reduced.add(max(counts))
             continue
         else:
+            # otherwise, do a decomposition pass with the smallest factor
             factor = sorted(counts.keys())[0]
         for k, v in replacements:
             factorization = []
+            # "divide out" `factor` from elements of existing decomposition
             for f in v:
+                # don't decompose factors we've already evaluated, and don't
+                # try to divide `factor` out of smaller factors (nonsensical)
                 if f in reduced or f <= factor:
                     factorization.append(f)
                     continue
                 factorization.append(factor)
                 difference = f - factor
-                while difference >= max(
-                    [e for e in extant if e != f]
-                ):
+                while difference >= max([e for e in extant if e != f]):
                     factorization.append(factor)
                     difference = difference - factor
                 if difference > 0:
@@ -126,7 +134,12 @@ def optimize_exponents(
             v[:] = factorization
         reduced.add(factor)
         extant = tuple(chain(*[r[1] for r in replacements]))
+    # this is a kind of set operation: we no longer care about number of
+    # occurrences
     replacements = {k: v for k, v in replacements}
+    # figure out which factors we'd like to predefine as variables, and what
+    # the "building blocks" of those variables are. 1 is a placeholder: we
+    # will never define it, but it's useful in this loop.
     variables = {1: [1]}
     for e in sorted(set(extant)):
         if e == 1:
@@ -140,22 +153,24 @@ def optimize_exponents(
             vfactor.append(pick)
             remainder -= pick
         variables[e] = vfactor
+    # remove the placeholder first-order variable
     variables.pop(1)
     return replacements, variables
 
 
 def force_line_precision(line: str, precision: Literal[16, 32, 64]) -> str:
     constructor = f"numpy.float{precision}"
-    # TODO: this is _not_ a general solution
     last, out = 0, ""
-    for match in re.finditer(r"([+* (-]+)([\d.]+)(e[+\-]?\d+)?.*?([+* )]|$)", line):
+    for match in re.finditer(
+        r"([+* (-]+)([\d.]+)(e[+\-]?\d+)?.*?([+* )]|$)", line
+    ):
         out += line[last:match.span()[0]]
         # don't replace exponents
         if match.group(1) == "**":
             out += line[slice(*match.span())]
         else:
             out += f"{match.group(1)}{constructor}({match.group(2)}"
-            if match.group(3) is not None:
+            if match.group(3) is not None:  # scientific notation
                 out += match.group(3)
             out += f"){match.group(4)}"
         last = match.span()[1]
@@ -188,6 +203,9 @@ def rewrite(
 
 
 def _rewrite_precomputed(polyexpr, varname, lines):
+    # replacements: what factors we will decompose each factor into
+    # variables: which factors we will define as variables, and their
+    # "building blocks"
     replacements, variables = optimize_exponents(regexponents(polyexpr))
     for k, v in variables.items():
         multiplicands = []
@@ -240,7 +258,7 @@ def quickseries(
     polyfunc = sp.lambdify(free, expr, ("scipy", "numpy"))
     # optionally, rewrite it to precompute stray powers and force precision
     polyfunc = rewrite(polyfunc, prefactor, bit_depth)
-    # optionally, jit it with numba
+    # optionally, convert it to a numbafied CPUDispatcher function
     if jit is True:
         import numba
         polyfunc = numba.njit(polyfunc)
